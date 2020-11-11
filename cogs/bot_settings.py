f8b962e36c32da9fffd31eef661e33fe47cd82be
SpicyTakis <joshpoodle42@gmail.com>
2020-11-10 21:24:28 -0500

    hi

--- cogs/bot_settings.py
+++ cogs/bot_settings.py
@@ -37,49 +37,6 @@ class BotSettings(utils.Cog):
         except utils.errors.InvokedMetaCommand:
             pass
 
-    @setup.command()
-    @utils.checks.meta_command()
-    async def vcmaxmembers(self, ctx:utils.Context):
-        """Run the bot setup"""
-
-        # Create settings menu
-        key_display_function = lambda k: getattr(ctx.bot.get_channel(k), 'mention', 'none')
-        menu = utils.SettingsMenuIterable(
-            'channel_list', 'channel_id', 'max_vc_members', 'MaxVCMembers',
-            commands.VoiceChannelConverter, "What voice channel do you want to set the max of?", key_display_function,
-            int, "How many members should be allowed in this VC?"
-        )
-        await menu.start(ctx)
-
-    @utils.group(enabled=False)
-    @commands.bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True)
-    @utils.cooldown.cooldown(1, 60, commands.BucketType.member)
-    @commands.guild_only()
-    async def usersettings(self, ctx:utils.Context):
-        """Run the bot setup"""
-
-        # Make sure it's only run as its own command, not a parent
-        if ctx.invoked_subcommand is not None:
-            return
-
-        # Create settings menu
-        menu = utils.SettingsMenu()
-        settings_mention = utils.SettingsMenuOption.get_user_settings_mention
-        menu.bulk_add_options(
-            ctx,
-            {
-                'display': lambda c: "Set setting (currently {0})".format(settings_mention(c, 'setting_id')),
-                'converter_args': [("What do you want to set the setting to?", "setting channel", commands.TextChannelConverter)],
-                'callback': utils.SettingsMenuOption.get_set_user_settings_callback('user_settings', 'setting_id'),
-            },
-        )
-        try:
-            await menu.start(ctx)
-            await ctx.send("Done setting up!")
-        except utils.errors.InvokedMetaCommand:
-            pass
-
-
 def setup(bot:utils.Bot):
     x = BotSettings(bot)
     bot.add_cog(x)
