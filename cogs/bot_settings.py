26a4896d9d29f00a2fb7c40055fd8da3ce1dc34e
SpicyTakis <joshpoodle42@gmail.com>
2020-11-24 21:08:59 -0500

    more mod features including more audit logging pog

--- cogs/bot_settings.py
+++ cogs/bot_settings.py
@@ -8,7 +8,7 @@ class BotSettings(utils.Cog):
     @commands.has_guild_permissions(manage_guild=True)
     @commands.bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True, manage_messages=True)
     @commands.guild_only()
-    async def setup2(self, ctx:utils.Context):
+    async def setup(self, ctx:utils.Context):
         """Talks the bot through a setup"""
 
         if ctx.invoked_subcommand is not None:
@@ -19,59 +19,16 @@ class BotSettings(utils.Cog):
             ctx,
             {
                 'display': "Moderation Settings",
-                'callback': self.bot.get_command("setup2 mod"),
+                'callback': self.bot.get_command("setup mod"),
             },
             {
                 'display': "Modlog Settings",
-                'callback': self.bot.get_command("setup2 modlog"),
+                'callback': self.bot.get_command("setup modlog"),
             },
             {
                 'display': "GeoPolitical Roleplay Settings",
-                'callback': self.bot.get_command("setup2 georp"),
+                'callback': self.bot.get_command("setup georp"),
             },
-        )
-        try:
-            await menu.start(ctx)
-            await ctx.send("Done setting up!")
-        except utils.errors.InvokedMetaCommand:
-            pass
-
-    @utils.group()
-    @commands.has_permissions(manage_guild=True)
-    @commands.bot_has_permissions(send_messages=True, embed_links=True, add_reactions=True)
-    @commands.guild_only()
-    async def setup(self, ctx:utils.Context):
-        """Run the bot setup"""
-
-        # Make sure it's only run as its own command, not a parent
-        if ctx.invoked_subcommand is not None:
-            return
-
-        # Create settings menu
-        menu = utils.SettingsMenu()
-        settings_mention = utils.SettingsMenuOption.get_guild_settings_mention
-        menu.bulk_add_options(
-            ctx,
-            {
-                'display': lambda c: "Set Worldnews channel (currently {0})".format(settings_mention(c, 'worldnews_channel')),
-                'converter_args': [("What do you want to set the Worldnews channel to?", "Worldnews Channel", commands.TextChannelConverter)],
-                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'worldnews_channel'),
-            },
-            {
-                'display': lambda c: "Set Spacenews channel (currently {0})".format(settings_mention(c, 'spacenews_channel')),
-                'converter_args': [("What do you want to set the Spacenews channel to?", "Spacenews Channel", commands.TextChannelConverter)],
-                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'spacenews_channel'),
-            },
-            {
-                'display': lambda c: "Set Audit Log channel (currently {0})".format(settings_mention(c, 'audit_channel')),
-                'converter_args': [("What do you want to set the Audit Log channel to?", "Audit Log Channel", commands.TextChannelConverter)],
-                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'audit_channel'),
-            },
-            {
-                'display': lambda c: "Set News notification role (currently {0})".format(settings_mention(c, 'news_role')),
-                'converter_args': [("What do you want to set News Notification Role to?", "News Notification Role", commands.RoleConverter)],
-                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'news_role'),
-            }
         )
         try:
             await menu.start(ctx)
@@ -79,8 +36,7 @@ class BotSettings(utils.Cog):
         except utils.errors.InvokedMetaCommand:
             pass
     
-
-    @setup2.command()
+    @setup.command()
     @utils.checks.meta_command()
     async def mod(self, ctx:utils.Context):
         """Run the bot setup"""
@@ -98,7 +54,12 @@ class BotSettings(utils.Cog):
                 'display': lambda c: "Set moderator role (currently {0})".format(settings_mention(c, 'guild_moderator_role_id')),
                 'converter_args': [("What do you want to set the moderator role to?", "moderator role", commands.RoleConverter)],
                 'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'guild_moderator_role_id'),
-            }
+            },
+            {
+                'display': lambda c: "Set mute role (currently {0})".format(settings_mention(c, 'muted_role_id')),
+                'converter_args': [("What do you want to set the mute role to?", "mute role", commands.RoleConverter)],
+                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('guild_settings', 'muted_role_id'),
+            },
         )
         try:
             await menu.start(ctx)
@@ -106,7 +67,7 @@ class BotSettings(utils.Cog):
         except utils.errors.InvokedMetaCommand:
             pass
 
-    @setup2.command()
+    @setup.command()
     @utils.checks.meta_command()
     async def modlog(self, ctx:utils.Context):
         """Talks the bot through a setup"""
@@ -157,7 +118,7 @@ class BotSettings(utils.Cog):
         except utils.errors.InvokedMetaCommand:
             pass
     
-    @setup2.command()
+    @setup.command()
     @utils.checks.meta_command()
     async def georp(self, ctx:utils.Context):
         """Run the bot setup"""
