6190fbfdb880943e170abda4179c6bc7b5c1fcba
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2020-12-14 15:53:57 -0500

    Remove this twat

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
 from cogs.utils import checks, errors
 from cogs.utils.checks import cooldown
-from cogs.utils.fursona import Fursona
 from cogs.utils.move_role import move_role_position_below
